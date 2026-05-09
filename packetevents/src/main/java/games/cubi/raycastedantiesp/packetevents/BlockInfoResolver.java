aae0060cd4afd308f2a75ca151e1247bb0d45356
Cubicake <74905@stpeters.sa.edu.au>
2026-05-09 21:26:26 +0930

    Add contract annotations

--- packetevents/src/main/java/games/cubi/raycastedantiesp/packetevents/BlockInfoResolver.java
+++ packetevents/src/main/java/games/cubi/raycastedantiesp/packetevents/BlockInfoResolver.java
@@ -1,6 +1,10 @@
 package games.cubi.raycastedantiesp.packetevents;
 
+import org.jetbrains.annotations.Contract;
+
 public interface BlockInfoResolver {
+    @Contract(pure = true)
     boolean isOccluding(int blockStateID);
+    @Contract(pure = true)
     boolean isTileEntity(int blockStateID);
 }
