d5a55ef3930710df3edfe644ee01984299c560ca
Cubicake <74905@stpeters.sa.edu.au>
2026-06-28 12:22:09 +0930

    Update leafpile and include it in PE module

--- packetevents/build.gradle.kts
+++ packetevents/build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
     implementation(project(":logging"))
     implementation(project(":core"))
 
+    compileOnly(project(":leafpile"))
+
     compileOnly("com.github.retrooper:packetevents-api:2.12.0")
     compileOnly("org.spongepowered:configurate-core:4.2.0")
     compileOnly("it.unimi.dsi:fastutil:8.5.18")
