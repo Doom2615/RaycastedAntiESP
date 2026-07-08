f226b53bb05b1a86e768facc39da37e9439ba594
Cubicake <70577278+Cubicake@users.noreply.github.com>
2026-07-08 16:54:35 +0930

    Support Folia/Canvas

--- platform-paper/build.gradle.kts
+++ platform-paper/build.gradle.kts
@@ -50,7 +50,7 @@ val javaToolchainService = project.extensions.getByType(JavaToolchainService::cl
 
 group = "games.cubi.raycastedantiesp.paper"
 
-val platformPaperVersion: String = "0.7.1-SNAPSHOT"
+val platformPaperVersion: String = "0.8.0-SNAPSHOT"
 val coreVersion = project(":core").version.toString()
 
 val commitShort = providers.exec {
--- platform-paper/src/main/java/games/cubi/raycastedantiesp/paper/utils/FoliaTicker.java
+++ platform-paper/src/main/java/games/cubi/raycastedantiesp/paper/utils/FoliaTicker.java
@@ -1,3 +1,11 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-only
+ * Copyright © 2026 Cubicake.
+ * This file is part of RaycastedAntiESP.
+ * RaycastedAntiESP is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License v3.0 only, which can be accessed at https://www.gnu.org/licenses/agpl-3.0.html.
+ * See README.md for warranty disclaimer and further information.
+ */
+
 package games.cubi.raycastedantiesp.paper.utils;
 
 import games.cubi.raycastedantiesp.paper.RaycastedAntiESP;
@@ -11,7 +19,7 @@ public class FoliaTicker implements IntSupplier {
     private final AtomicInteger tick = new AtomicInteger(0);
 
     public FoliaTicker() {
-        Bukkit.getGlobalRegionScheduler().runAtFixedRate(RaycastedAntiESP.get(), this::increment, 0L, 1L); //Is this guaranteed to be a specific thread?
+        Bukkit.getGlobalRegionScheduler().runAtFixedRate(RaycastedAntiESP.get(), this::increment, 1L, 1L); //Is this guaranteed to be a specific thread?
     }
 
     private void increment(ScheduledTask scheduledTask) {
