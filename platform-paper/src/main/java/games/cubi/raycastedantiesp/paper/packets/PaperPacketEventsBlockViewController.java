4f086b4d6a0fbc19eb59635754c879771242b16a
Cubicake <74905@stpeters.sa.edu.au>
2026-07-14 19:02:10 +0930

    New params

--- platform-paper/src/main/java/games/cubi/raycastedantiesp/paper/packets/PaperPacketEventsBlockViewController.java
+++ platform-paper/src/main/java/games/cubi/raycastedantiesp/paper/packets/PaperPacketEventsBlockViewController.java
@@ -2,6 +2,7 @@ package games.cubi.raycastedantiesp.paper.packets;
 
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.event.PacketListenerPriority;
+import games.cubi.raycastedantiesp.core.chunks.BlockInfoResolver;
 import games.cubi.raycastedantiesp.packetevents.viewcontrollers.PacketEventsBlockViewController;
 import io.github.retrooper.packetevents.util.SpigotConversionUtil;
 import org.bukkit.Material;
@@ -12,8 +13,8 @@ public class PaperPacketEventsBlockViewController extends PacketEventsBlockViewC
     private final int stoneBlockId = SpigotConversionUtil.fromBukkitBlockData(Material.STONE.createBlockData()).getGlobalId();
     private final int deepslateBlockId = SpigotConversionUtil.fromBukkitBlockData(Material.DEEPSLATE.createBlockData()).getGlobalId();
 
-    public PaperPacketEventsBlockViewController(IntSupplier currentTickSupplier) {
-        super(new PacketEventsPaperBlockInfoResolver(), currentTickSupplier);
+    public PaperPacketEventsBlockViewController(BlockInfoResolver blockInfoResolver, boolean trackAllBlocks, IntSupplier currentTickSupplier) {
+        super(blockInfoResolver, trackAllBlocks, currentTickSupplier);
         PacketEvents.getAPI().getEventManager().registerListener(this, PacketListenerPriority.HIGHEST);
     }
 
