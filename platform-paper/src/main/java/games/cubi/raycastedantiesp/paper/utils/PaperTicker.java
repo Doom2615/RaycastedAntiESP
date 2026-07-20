f8a3f09c0738e152fd0c574fc4759274ff0f3651
Cubicake <74905@stpeters.sa.edu.au>
2026-07-20 18:55:34 +0930

    Opaque tick reads and writes

--- platform-paper/src/main/java/games/cubi/raycastedantiesp/paper/utils/PaperTicker.java
+++ platform-paper/src/main/java/games/cubi/raycastedantiesp/paper/utils/PaperTicker.java
@@ -1,23 +1,26 @@
 package games.cubi.raycastedantiesp.paper.utils;
 
 import com.destroystokyo.paper.event.server.ServerTickStartEvent;
+import games.cubi.raycastedantiesp.core.utils.VarHandler;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 
+import java.lang.invoke.VarHandle;
 import java.util.function.IntSupplier;
 
 public class PaperTicker extends PaperListener implements IntSupplier {
-    private volatile int currentTick = 0;
+    private volatile int currentTick; private static final VarHandle CURRENT_TICK = VarHandler.get(PaperTicker.class, "currentTick", int.class);
 
     public PaperTicker() {}
 
     @Override
     public int getAsInt() {
-        return currentTick;
+        return (int) CURRENT_TICK.getOpaque(this);
     }
 
     @EventHandler(priority = EventPriority.LOWEST) //Runs first
     public void serverTickStartEvent(ServerTickStartEvent event) {
-        currentTick++;
+        int current = (int) CURRENT_TICK.getOpaque(this); //this runs on the paper tick thread so there's no race possible
+        CURRENT_TICK.setOpaque(this, current + 1);
     }
 }
