6de11030b0546bb910602b73cba1df6819042b15
Cubicake <74905@stpeters.sa.edu.au>
2026-08-26 21:04:42 +0930

    Bump version

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     testImplementation("it.unimi.dsi:fastutil:8.5.18")
 }
 
-val coreVersion = "0.7.1-SNAPSHOT"
+val coreVersion = "0.7.2-SNAPSHOT"
 
 val isRelease = gradle.startParameter.taskNames.any {
     it.contains("buildRelease")
--- platform-paper/build.gradle.kts
+++ platform-paper/build.gradle.kts
@@ -64,7 +64,7 @@ val javaToolchainService = project.extensions.getByType(JavaToolchainService::cl
 
 group = "games.cubi.raycastedantiesp.paper"
 
-val platformPaperVersion: String = "0.10.1-SNAPSHOT"
+val platformPaperVersion: String = "0.10.2-SNAPSHOT"
 val coreVersion = project(":core").version.toString()
 
 val commitShort = providers.exec {
